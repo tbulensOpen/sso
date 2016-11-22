b7ebc8a45813eb8e319017f90f99ccbd826910dc
tbulensOpen <tbulens@msn.com>
2016-11-21 21:15:15 -0600

    got login page to show up, still have permissions problem with rabbitMQ

--- groovy/sso-login-web-stub-groovy/src/main/groovy/org/tbulens/sso/loginweb/Application.groovy
+++ groovy/sso-login-web-stub-groovy/src/main/groovy/org/tbulens/sso/loginweb/Application.groovy
@@ -1,9 +1,12 @@
 package org.tbulens.sso.loginweb
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.ImportResource
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @ImportResource("classpath:contexts/application-context.xml")
